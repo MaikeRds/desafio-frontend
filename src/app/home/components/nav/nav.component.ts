9885775a4e6980dbc79fc7ad2d8dea54d3603d3d
Maike Rodrigues <maikerodrigues@cialne.com.br>
2020-11-06 16:26:03 -0300

    Ajuste navbar

--- src/app/home/components/nav/nav.component.ts
+++ src/app/home/components/nav/nav.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { AuthService } from 'src/app/auth/shared/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent implements OnInit {
+export class NavComponent {
 
-  constructor() { }
+  hasBackdrop = true;
+  isHandset= 'side';
+  sidenavMode = 'side';
 
-  ngOnInit(): void {
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  .pipe(
+    map(result => result.matches),
+    shareReplay()
+  );
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private authService:AuthService,
+    private router: Router
+  ) {}
+
+
+  logout(){
+    this.authService.logout();
+    this.router.navigate(['']);
   }
 
 }
