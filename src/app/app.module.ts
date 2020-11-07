03801986982329e7b68f0a97ce7e2f6aa8f8a5ce
Maike Rodrigues <maikerodrigues@cialne.com>
2020-11-07 15:20:26 +0000

    Criado interceptor para injetar o token em req.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { httpInterceptorProviders } from './auth/shared/http-interceptors';
 import { AuthModule } from './auth/auth.module';
 import { EstabelecimentoModule } from './estabelecimento/estabelecimento.module';
 import { HomeModule } from './home/home.module';
@@ -7,6 +8,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +22,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HomeModule,
     EstabelecimentoModule
   ],
-  providers: [],
+  providers: [httpInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
